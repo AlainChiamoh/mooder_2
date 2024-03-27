22c88ca95b4a27ed81ed452e172135280e40a0a1
Daniel Nganeck <nganeckdaniel@gmail.com>
2024-03-27 14:35:12 +0100

    Implement the FlagContainer and the Flag components

--- src/components/Flag.jsx
+++ src/components/Flag.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Flag = ({flag}) => {
   return (
-    <div>Flag</div>
+    <div className='flex flex-col justify-center items-center gap-1'>
+      <div className='w-5 h-5 rounded-full overflow-hidden'>
+      <img src={flag?.image} width={30} height={30} className=' object-cover w-full h-full'/>
+      </div>
+      <p className='text-[10px] font-bold'>
+        {flag?.country}
+      </p>
+    </div>
   )
 }
 
