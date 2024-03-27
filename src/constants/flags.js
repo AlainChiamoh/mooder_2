7b84b51143fbefd417ab8a62c7d0469345e17adc
Frank <alainchiamoh@gmail.com>
2024-03-27 05:40:52 -0700

    add constants

---
+++ src/constants/flags.js
@@ -0,0 +1,46 @@
+export default flag = [
+    {
+        country: 'Cameroon',
+        image: require('../assets/image/cameroon_image.png'),
+    },
+    {
+        country: 'Cameroon',
+        image: require('../assets/image/cameroon_image.png'),
+    },
+    {
+        country: 'Cameroon',
+        image: require('../assets/image/cameroon_image.png'),
+    },
+    {
+        country: 'Cameroon',
+        image: require('../assets/image/cameroon_image.png'),
+    },
+    {
+        country: 'Cameroon',
+        image: require('../assets/image/cameroon_image.png'),
+    },
+    {
+        country: 'Cameroon',
+        image: require('../assets/image/cameroon_image.png'),
+    },
+    {
+        country: 'Cameroon',
+        image: require('../assets/image/cameroon_image.png'),
+    },
+    {
+        country: 'Cameroon',
+        image: require('../assets/image/cameroon_image.png'),
+    },
+    {
+        country: 'Cameroon',
+        image: require('../assets/image/cameroon_image.png'),
+    },
+    {
+        country: 'Cameroon',
+        image: require('../assets/image/cameroon_image.png'),
+    },
+    {
+        country: 'Cameroon',
+        image: require('../assets/image/cameroon_image.png'),
+    },
+]
